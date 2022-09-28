e161de72acfe4be649fe0e3ef2837681c3791dee
stephlamas <esteflamas@gmail.com>
2022-09-28 17:05:16 +0200

    playing around

---
+++ src/data/data-sources/mongodb/mongodb-data-source.ts
@@ -0,0 +1,7 @@
+import { Dog } from "../../../domain/models/Dog";
+import { UserDataSource } from "../interfaces/data-sources/UserDataSource";
+
+export default class MongoDBDataSource implements UserDataSource {
+  
+}
+
